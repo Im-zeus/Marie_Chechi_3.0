c1876c921b1a1f526499ecff2b743edd43d29007
Im_Zeus <87465532+Im-zeus@users.noreply.github.com>
2021-08-26 16:05:28 +0530

    Update special.py

--- tg_bot/modules/special.py
+++ tg_bot/modules/special.py
@@ -104,19 +104,16 @@ def leavechat(bot: Bot, update: Update, args: List[int]):
 __help__ = """
 **Owner only:**
 - /getlink **chatid**: Get the invite link for a specific chat.
-- /banall: Ban all members from a chat
 - /leavechat **chatid** : leave a chat
 **Sudo/owner only:**
 - /quickscope **chatid** **userid**: Ban user from chat.
 - /quickunban **chatid** **userid**: Unban user from chat.
-- /snipe **chatid** **string**: Make me send a message to a specific chat.
 - /rban **chatid** **userid** remotely ban a user from a chat
 - /runban **chatid** **userid** remotely unban a user from a chat
 - /Stats: check bot's stats
 - /chatlist: get chatlist
 - /gbanlist: get gbanned users list
 - /gmutelist: get gmuted users list
-- Chat bans via /restrict chat_id and /unrestrict chat_id commands
 **Support user:**
 - /Gban : Global ban a user
 - /Ungban : Ungban a user
